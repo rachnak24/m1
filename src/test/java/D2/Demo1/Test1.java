dad53d5fd1bfae92ebe62ad6b125ca5815c1f46f
rachna <rachna.khanna@live.com>
2020-02-04 22:21:17 -0600

    .4rtty

---
+++ src/test/java/D2/Demo1/Test1.java
@@ -0,0 +1,21 @@
+package D2.Demo1;
+
+import org.testng.annotations.Test;
+
+public class Test1
+{
+	@Test
+	public void Carlogin()
+	{
+		System.out.println("i bought a car");
+	}
+	
+	@Test
+	public void Mobilelogin() 
+	{
+		System.out.println("my new mobile is here");
+	}
+	
+	
+	
+}
---
+++ src/test/java/D2/Demo1/Test2.java
@@ -0,0 +1,26 @@
+package D2.Demo1;
+
+import org.testng.annotations.Test;
+
+public class Test2
+{
+	
+	@Test
+	public void PersonalLoan()
+	{
+		System.out.println("your loan is approved");
+	}
+
+	@Test
+	public void Homeloan() 
+	{
+		System.out.println();
+	}
+	
+	
+	
+	
+	
+	
+	
+}
